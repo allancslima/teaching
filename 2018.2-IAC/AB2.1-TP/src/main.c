db3506908b6bb6b6949806f13fc2a138ad35ddc1
Allan Lima <allancslima@gmail.com>
2019-02-10 00:30:08 -0300

    Standardize code

--- 2018.2-IAC/AB2.1-TP/src/main.c
+++ 2018.2-IAC/AB2.1-TP/src/main.c
@@ -14,42 +14,39 @@
 #define CMD_CPU_USAGE_FORMAT "ps u %d | awk '{print $3}' | grep -v CPU"
 #define CMD_KILL_PROCESS_FORMAT "kill %d"
 
-void displayProcessCpuUsage(int pid)
+void display_process_cpu_usage(int pid)
 {
 	char bash_cmd[256];
 	sprintf(bash_cmd, CMD_CPU_USAGE_FORMAT, pid);
 	
 	FILE* pipe = popen(bash_cmd, "r");
 
-	if (pipe == NULL)
-	{
+	if (pipe == NULL) {
 		printf("Error!");
 		exit(-1);
 	}
 
 	char buffer[8];
-	char* cpuUsage = fgets(buffer, sizeof(buffer), pipe);
+	char* cpu_usage = fgets(buffer, sizeof(buffer), pipe);
 	pclose(pipe);
 
-	printf("CPU: %% %s\n", cpuUsage);
+	printf("CPU: %% %s\n", cpu_usage);
 }
 
-void trackProcessResourcesUsage(int pid, int timeInSeconds, int trackMemory)
+void track_process_resources_usage(int pid, int time_in_seconds, int track_memory)
 {
-	time_t currentTime = time(NULL);
-	time_t finalTime = currentTime + timeInSeconds + 1;
-	time_t seconds = currentTime;
-	struct tm* timeInfo;
+	time_t current_time = time(NULL);
+	time_t final_time = current_time + time_in_seconds + 1;
+	time_t seconds = current_time;
+	struct tm* time_info;
 
-	while (currentTime < finalTime)
-	{
-		currentTime = time(NULL);
-		if (currentTime != seconds)
-		{
-			seconds = currentTime;
-			timeInfo = localtime(&seconds);
-			printf("%s", asctime(timeInfo));
-			displayProcessCpuUsage(pid);
+	while (current_time < final_time) {
+		current_time = time(NULL);
+		if (current_time != seconds) {
+			seconds = current_time;
+			time_info = localtime(&seconds);
+			printf("%s", asctime(time_info));
+			display_process_cpu_usage(pid);
 		}
 	}
 	char cmd_kill_process[32];
@@ -57,12 +54,12 @@ void trackProcessResourcesUsage(int pid, int timeInSeconds, int trackMemory)
 	system(cmd_kill_process);
 }
 
-void consumeCPU()
+void consume_cpu()
 {
 	for (;;) {}
 }
 
-void consumeCPUAndMemory()
+void consume_cpu_and_memory()
 {
 
 }
@@ -71,31 +68,24 @@ int main (int argc, char *argv[])
 {
 	int pid = fork();
 
-	if (pid < 0)
-	{
+	if (pid < 0) {
 		printf("An error occurred during fork process!");
 		exit(-1);
 	}
-	else if (pid > 0)
-	{
-		if (strcmp(argv[1], ARG_CPU) == 0)
-		{
-			trackProcessResourcesUsage(pid, DEFAULT_TIME_TRACKING, 0);
+	else if (pid > 0) {
+		if (strcmp(argv[1], ARG_CPU) == 0) {
+			track_process_resources_usage(pid, DEFAULT_TIME_TRACKING, 0);
 		}
-		else if (strcmp(argv[1], ARG_CPU_AND_MEMORY) == 0)
-		{
-			trackProcessResourcesUsage(pid, DEFAULT_TIME_TRACKING, 1);
+		else if (strcmp(argv[1], ARG_CPU_AND_MEMORY) == 0) {
+			track_process_resources_usage(pid, DEFAULT_TIME_TRACKING, 1);
 		}
 	}
-	else
-	{
-		if (strcmp(argv[1], ARG_CPU) == 0)
-		{
-			consumeCPU();
+	else {
+		if (strcmp(argv[1], ARG_CPU) == 0) {
+			consume_cpu();
 		}
-		else if (strcmp(argv[1], ARG_CPU_AND_MEMORY) == 0)
-		{
-			consumeCPUAndMemory();
+		else if (strcmp(argv[1], ARG_CPU_AND_MEMORY) == 0) {
+			consume_cpu_and_memory();
 		}
 	}
 	return 0;
